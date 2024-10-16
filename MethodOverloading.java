9661cb84c3da01a243529c93c76e104ab7435b64
Manikanta599 <manikantamutyala854@gmail.com>
2024-10-16 20:43:17 +0530

    first

---
+++ MethodOverloading.java
@@ -0,0 +1,41 @@
+//BY CHANGING NUMBER OF ARGUEMNTS
+//BY CHANGING THE DATA TYPE
+
+/**
+ * MethodOverloading
+ */
+/* public class MethodOverloading { 
+
+    static int add(int a,int b)
+    {
+        return a+b;
+    }
+
+    static int add(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+
+
+    public static void main(String[] args) {
+        add(2, 4);
+        add(4, 5,6);
+    }
+}
+    */
+
+/**
+ * MethodOverloading
+ */
+public class MethodOverloading {
+
+    static int add(int a,int b)
+    {
+        return a+b;
+    }
+
+    static double add(double a,int b)
+    {
+        return a+b;
+    }
+}
